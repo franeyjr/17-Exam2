55f637bf2053b4bf7ec3ea90a88344a99701c695
franeyjr <franeyjr@rose-hulman.edu>
2019-01-15 20:32:25 -0500

    NOT DONE

--- src/problem1.py
+++ src/problem1.py
@@ -14,8 +14,8 @@ def main():
     """ Calls the   TEST   functions in this module. """
     run_test_problem1a()
     run_test_problem1b()
-    # run_test_problem1c()
-    # run_test_problem1d()
+    run_test_problem1c()
+    run_test_problem1d()
 
 
 ###############################################################################
@@ -393,10 +393,13 @@ def problem1c(integers):
            their respective indices (0, 1, and 2, respectively).
      """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     ###########################################################################
-
+    for k in range(len(integers)):
+        if integers[k] < k:
+            return integers[k]
+    else: return 999
 
 def run_test_problem1d():
     """ Tests the   problem1d   function. """
@@ -530,7 +533,12 @@ def problem1d(t, sequence):
     # TODO: 4. Implement and test this function.
     #          Tests have been written for you (above).
     ###########################################################################
-
+    product = 1
+    n = len(sequence) //2
+    for k in range(len(sequence)-1,len(sequence)-1-n,-1):
+        if sum_of_digits(sequence[k]) > t:
+            product = product * sequence[k]
+    return product
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
